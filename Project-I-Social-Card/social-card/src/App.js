8086296343eaba66e36c9549d41aa2cfd01e16f1
Myke47 <mychel.larson@gmail.com>
2019-05-09 06:38:46 -0700

    footer and mostly completed

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
+import { library } from '@fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faShare } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStroopwafel,faArrowUp,faArrowDown,faBookmark,faShare);
 
 const App = () => {
   return (
     <div className= "app">
     <HeaderContainer />
     <CardContainer />
+    <Footer />
 
     </div>
   );
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Footer = () => {
+    return (
+        <div className="footbox">
+        <div className="comment">
+        <FontAwesomeIcon 
+            icon="arrow-up"
+            color="crimson"
+            size="3x"
+        />
+                    {/* {' '}Upvote */}
+        </div>
+        <div className="shares">
+        <FontAwesomeIcon 
+            icon="arrow-down"
+            color="crimson"
+            size="3x"
+        />
+
+        </div>
+        <div className="saves">
+        <FontAwesomeIcon 
+            icon="bookmark"
+            color="crimson"
+            size="3x"
+        />
+
+        </div>
+        <div className="send">
+        <FontAwesomeIcon 
+            icon="share"
+            color="crimson"
+            size="3x"
+        />
+
+        </div>
+
+        </div>
+    );
+};
+
+export default Footer;
